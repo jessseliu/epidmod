8e87ad653930d6a3ea00023078da45a3081d4355
Yingzhe Liu <l_yzzz@163.com>
2020-07-09 01:14:44 +0800

    update namespace

---
+++ R/epidmod.R
@@ -0,0 +1,12 @@
+#' epidmod: A package for
+#'
+#' The epidmod package provides categories of important functions:
+#' rSIR
+#'
+#' @section epidmod functions:
+#' @importFrom stats rexp runif
+
+#'
+#' @docType package
+#' @name epidmod
+NULL
