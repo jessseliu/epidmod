35d38c880a34c9e68fe12f3ed154ee809af9f6ff
Yingzhe Liu <l_yzzz@163.com>
2020-07-09 01:01:29 +0800

    update

---
+++ R/NULL.R
@@ -0,0 +1,11 @@
+#' epidmod: A package for
+#'
+#' The epidmod package provides categories of important functions:
+#' rSIR
+#'
+#' @section epidmod functions:
+
+#'
+#' @docType package
+#' @name epidmod
+NULL
